da2b8818bc005019e480a46c122251fd090306ef
Mohammed Yehia <m.yehia@dev-gate.com>
2016-02-06 01:23:38 +0300

    Documented the class

--- HTTP/request.php
+++ HTTP/request.php
@@ -4,10 +4,24 @@ namespace Lily\HTTP;
 final class Request
 {
 
+    /**
+     * @var object The POST RAW DATA
+     */
     private $_POSTData;
+
+    /**
+     * @var object The GET SUPER GLOBAL ARRAY DATA
+     */
     private $_GETData;
+
+    /**
+     * @var object The SERVER SUPER GLOBAL ARRAY DATA
+     */
     private $_SERVERData;
 
+    /**
+     * Request constructor.
+     */
     public function __construct()
     {
         if($this->isPost()) {
@@ -21,6 +35,10 @@ final class Request
         $this->setServerData();
     }
 
+    /**
+     * Check wither the request is an AJAX Request
+     * @return bool
+     */
     public function isXMLHTTPRequest()
     {
         if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
@@ -30,21 +48,36 @@ final class Request
         return false;
     }
 
+    /**
+     * Get the REFERER header
+     * @return mixed
+     */
     public function getReferer()
     {
         return $_SERVER['HTTP_REFERER'];
     }
 
+    /**
+     * Check wither the request is a POST request
+     * @return bool
+     */
     public function isPost()
     {
         return isset($_POST) && !empty($_POST);
     }
 
+    /**
+     * Check wither the request is a POST request
+     * @return bool
+     */
     public function isGet()
     {
         return isset($_GET) && !empty($_GET);
     }
 
+    /**
+     * Sets the POST RAW Data
+     */
     public function setPostData()
     {
         if($this->isPost()) {
@@ -57,6 +90,9 @@ final class Request
         }
     }
 
+    /**
+     * Sets the GET Object Data
+     */
     public function setGetData()
     {
         if($this->isGet()) {
@@ -64,11 +100,19 @@ final class Request
         }
     }
 
+    /**
+     * Sets the SERVER object Data
+     */
     public function setServerData()
     {
         $this->_SERVERData = (object) $_SERVER;
     }
 
+    /**
+     * Returns a given value from the POST DATA
+     * @param $key
+     * @return mixed
+     */
     public function getPostValue($key)
     {
         if(!property_exists($this->_POSTData, $key)) {
@@ -77,6 +121,11 @@ final class Request
         return $this->_POSTData->$key;
     }
 
+    /**
+     * Returns a given value from the GET DATA
+     * @param $key
+     * @return mixed
+     */
     public function getQueryValue($key)
     {
         if(!property_exists($this->_GETData, $key)) {
@@ -85,6 +134,11 @@ final class Request
         return $this->_GETData->$key;
     }
 
+    /**
+     * Returns a given value from the SERVER DATA
+     * @param $key
+     * @return mixed
+     */
     public function getServerValue($key)
     {
         if(!property_exists($this->_SERVERData, $key)) {
@@ -93,21 +147,39 @@ final class Request
         return $this->_SERVERData->$key;
     }
 
+    /**
+     * Check wither a value exists in the POST RAW DATA
+     * @param $property
+     * @return bool
+     */
     public function postHas($property)
     {
         return property_exists($this->_POSTData, $property);
     }
 
+    /**
+     * Check wither a value exists in the GET DATA
+     * @param $property
+     * @return bool
+     */
     public function getHas($property)
     {
         return property_exists($this->_GETData, $property);
     }
 
+    /**
+     * Returns the URL PATH
+     * @return mixed
+     */
     public function getPath()
     {
         return parse_url($this->getServerValue('REQUEST_URI'), PHP_URL_PATH);
     }
 
+    /**
+     * Returns the HTTP Request Method
+     * @return mixed
+     */
     public function getMethod()
     {
         return $this->_SERVERData->REQUEST_METHOD;
